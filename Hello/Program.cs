4c06daff762a4e231640ee118ea839c603ff4bfe
sadek121 <sadekarek@wp.pl>
2018-02-05 18:29:04 +0100

    klasa I.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Hello
 {
@@ -10,6 +7,75 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            ValueTypes();
+            for (; ;)
+            {
+                Greeting();
+                Age();
+                Settings();
+
+            }
+
+
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+            Console.WriteLine("maxInt =" + maxInt);
+            Console.WriteLine("minInt =" + minInt);
+            Console.WriteLine("maxLong =" + maxLong);
+            Console.WriteLine("minlong =" + minLong);
+        }
+
+        /// <summary>
+        /// Ustawienia
+        /// </summary>
+        private static void Settings()
+        {
+            System.Console.ReadKey();
+            System.Console.Clear();
+            System.Console.ResetColor();
+        }
+        /// <summary>
+        /// Wypisujemy wiek użytkownika
+        /// </summary>
+        private static void Age()
+        {
+            System.Console.Write("Wpisz ile masz lat: ");
+
+            int age;
+            bool result = int.TryParse(System.Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                System.Console.ForegroundColor = System.ConsoleColor.Cyan;
+                System.Console.WriteLine(" Brawo. Masz ponad 18 lat możesz wypić browara!");
+            }
+
+            else if (result == false)
+            {
+                System.Console.WriteLine("Wpisałeś zły wiek");
+            }
+            else
+            {
+                System.Console.ForegroundColor = System.ConsoleColor.DarkMagenta;
+                System.Console.WriteLine(" Masz wypij Mleko");
+            }
+        }
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            System.Console.Write("Wpisz swoje imię: ");
+            string name = System.Console.ReadLine();
+
+            System.Console.WriteLine("Witaj " + name);
+            
         }
     }
 }
---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan (zmienne - pola)
+        List<float> ratings;
+        //Zachowania
+        public void Rating(float rating)
+        {
+            ratings.Add(rating);
+        }
+        public float CallculateAverge()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+        public float GiveMaxRatings()
+        {
+            return ratings.Max();
+        }
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
